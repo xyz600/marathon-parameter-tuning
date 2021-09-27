77cd2c90ac5a03031ae8ea242154a9ad25178eab
xyz600 <xyz600600@gmail.com>
2021-09-27 19:37:57 +0900

    add first init

---
+++ config.py
@@ -0,0 +1,82 @@
+import os
+import yaml
+from enum import Enum
+from typing import List
+
+
+class Scale(Enum):
+    LINEAR = 0,
+    LOG = 1
+
+    def construct(scale_str: str):
+        if scale_str == "log":
+            return Scale.LOG
+        elif scale_str == "linear":
+            return Scale.LINEAR
+        else:
+            raise ValueError("inappropreate scale parameter")
+
+
+class Type(Enum):
+    FLOAT = 0,
+    INT = 1
+
+    def construct(type_str: str):
+        if type_str == "float":
+            return Type.FLOAT
+        elif type_str == "int":
+            return Type.INT
+        else:
+            raise ValueError("inappropreate type parameter")
+
+
+class Param:
+    def __init__(self):
+        self.name: str = ""
+        self.type: Type = Type.FLOAT
+        self.range_from: float = 0.0
+        self.range_to: float = 1.0
+        self.scale: Scale = Scale.LINEAR
+
+
+class Config:
+
+    def _validate(self):
+
+        for param in self.param_list:
+
+            assert(param.range_from < param.range_to)
+            if param.scale == Scale.LOG:
+                assert(0 < param.range_from)
+                assert(0 < param.range_to)
+
+        assert(os.path.exists(self.dataset_template.format(0)))
+        assert(os.path.exists(self.dataset_template.format(self.data_size - 1)))
+        assert(os.path.exists(self.exec_path))
+
+    def __init__(self, yaml_filepath: str):
+        with open(yaml_filepath) as fin:
+            obj = yaml.safe_load(fin)
+
+            self.param_list: List[Param] = []
+
+            for param_yml in obj["param_list"]:
+                param = Param()
+                param.name = param_yml["name"]
+                param.type = Type.construct(param_yml["type"])
+                if param.type == Type.FLOAT:
+                    param.range_from = float(param_yml["range_from"])
+                    param.range_to = float(param_yml["range_to"])
+                else:
+                    param.range_from = int(param_yml["range_from"])
+                    param.range_to = int(param_yml["range_to"])
+                param.scale = Scale.construct(param_yml["scale"])
+                self.param_list.append(param)
+
+            self.dataset_template = obj["dataset_template"]
+            self.number_of_iteration = int(obj["number_of_iteration"])
+            self.dataset_size = int(obj["dataset_size"])
+            self.exec_path = obj["exec_path"]
+            self.parallel_job_size = obj["parallel_job_size"]
+
+        self._validate()
---
+++ optimize.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+import optuna
+import subprocess
+import tempfile
+import sys
+from config import Config, Type, Scale, Param
+
+
+class Evaluator:
+
+    def __init__(self, config: Config):
+        self.config = config
+
+    def get_filepath(self, root_path, idx):
+        return "{}/1/{}/stderr".format(root_path, idx)
+
+    def get_score(self, root_path, idx):
+        with open(self.get_filepath(root_path, idx), 'r') as fin:
+            return float(fin.readlines()[-1].strip())
+
+    def collect_result(self, root_path):
+        result = []
+        for i in range(self.config.dataset_size):
+            result.append(self.get_score(root_path, i))
+        return result
+
+    def __suggest(self, trial: optuna.Trial, param: Param):
+        if param.type == Type.FLOAT:
+            if param.scale == Scale.LOG:
+                return trial.suggest_loguniform(
+                    param.name, param.range_from, param.range_to)
+            else:
+                return trial.suggest_uniform(
+                    param.name, param.range_from, param.range_to)
+        else:
+            return trial.suggest_uniformint(
+                param.name, param.range_from, param.range_to)
+
+    def __evaluate(self, trial: optuna.Trial):
+        params = []
+        for param in self.config.param_list:
+            params.append(self.__suggest(trial, param))
+
+        tmpdir = tempfile.TemporaryDirectory()
+        # do experiment
+        parallel_arg_list = ['parallel', '--progress',
+                             '-j11', '--silent', '--result', tmpdir.name]
+        exec_arg_list = [self.config.exec_path] + list(map(str, params))
+        input_redirect_arg_list = ["<", self.config.dataset_template]
+        dataset_range_arg_list = [":::"] + \
+            list(map(str, range(self.config.dataset_size)))
+
+        subprocess.run(parallel_arg_list + exec_arg_list +
+                       input_redirect_arg_list +
+                       dataset_range_arg_list,
+                       stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        result = self.collect_result(tmpdir.name)
+        tmpdir.cleanup()
+
+        return sum(result) / len(result)
+
+    def doit(self):
+        study = optuna.create_study()
+        study.optimize(self.__evaluate, n_trials=self.config.number_of_iteration,
+                       n_jobs=self.config.parallel_job_size)
+
+
+if __name__ == "__main__":
+
+    config = Config("config.yml")
+
+    evaluator = Evaluator(config)
+    evaluator.doit()
